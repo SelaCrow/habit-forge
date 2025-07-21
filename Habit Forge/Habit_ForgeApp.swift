9112f5ebe4f105696971b0860efadf98e49418cc
marisela <mariselaalejandragomez@gmail.com>
2025-07-21 16:52:56 -0700

    Initial Commit

---
+++ Habit Forge/Habit_ForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_ForgeApp.swift
+//  Habit Forge
+//
+//  Created by Marisela Gomez on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_ForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit ForgeTests/Habit_ForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_ForgeTests.swift
+//  Habit ForgeTests
+//
+//  Created by Marisela Gomez on 7/21/25.
+//
+
+import Testing
+@testable import Habit_Forge
+
+struct Habit_ForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
