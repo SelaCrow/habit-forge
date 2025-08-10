89cc457b20de58ef56860c3fbb634bf75b2811d0
marisela <mariselaalejandragomez@gmail.com>
2025-08-09 19:16:58 -0700

    please work

---
+++ Habit Forge/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  Habit Forge
+//
+//  Created by Marisela Gomez on 8/9/25.
+//
+import Foundation
+
+enum Secrets {
+    static let OPENAI_API_KEY = "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"
+}
+
+//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
